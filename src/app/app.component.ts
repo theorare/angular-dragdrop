b556b90a66317a9295bf8f90ff1c205e0e2d603a
Bharath B <bharath@niyamaglobal.com>
2020-08-05 22:23:16 +0530

    Drag drop initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+ import { DragDropService } from './app.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  //implements OnInit
   title = 'angular-dragdrop';
+  public personaldetails = [];
+   constructor(private ddService: DragDropService) {}
+   ngOnInit() {
+      this.ddService.getData().subscribe((data) => {
+         this.personaldetails = Array.from(Object.keys(data), k=>data[k]);
+         console.log(this.personaldetails);
+      });
+   }
+
+   onDrop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+       moveItemInArray(event.container.data, 
+          event.previousIndex, event.currentIndex);
+    } else {
+       transferArrayItem(event.previousContainer.data,
+       event.container.data,
+       event.previousIndex,
+       event.currentIndex);
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+//import { ScrollDispatchModule } from '@angular/cdk/scrolling';
+// Drag drop
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { DragDropService } from './app.service';
+
+// import { CdkStepperModule } from '@angular/cdk/stepper';
+// import { CdkTableModule } from '@angular/cdk/table';
+import { CdkTreeModule } from '@angular/cdk/tree';
+
+
 
 @NgModule({
   declarations: [
@@ -10,9 +21,24 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    //ScrollDispatchModule,
+    DragDropModule
+    ,
+    // CdkStepperModule,
+    // CdkTableModule,
+    CdkTreeModule
   ],
-  providers: [],
+  // exports: [
+  //   AppComponent,
+  //   DragDropModule
+  //   ,
+  //   // CdkStepperModule,
+  //   // CdkTableModule,
+  //   CdkTreeModule
+  // ],
+  providers: [DragDropService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
